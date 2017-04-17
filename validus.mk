6877f18eacc5a3485f5fe2eed563bdbfab916ae1
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2017-04-17 10:15:08 -0400

    add myself

--- validus.mk
+++ validus.mk
@@ -2,5 +2,9 @@ $(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/klte/full_klte.mk)
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Samsung Galaxy S5" \
+    DEVICE_MAINTAINERS="Anthony Pyrtle(Yayo)" \
+
 PRODUCT_DEVICE := klte
 PRODUCT_NAME := validus_klte
