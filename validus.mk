334e7eae865ce5e097d22ee2ec65d8f35e085f65
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2017-04-17 09:34:27 -0400

    val-ify

---
+++ validus.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/klte/full_klte.mk)
+
+PRODUCT_DEVICE := klte
+PRODUCT_NAME := validus_klte
